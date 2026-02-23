adf3f344ce7cd7894a6f3b2b127d4e5118e2e201
Paula Alonso <mjsafr951753@gmail.com>
2026-02-23 13:35:44 +0100

    Realizando ejercicio4

---
+++ ejercicio4.java
@@ -0,0 +1,16 @@
+public class prog4{
+	double pvp;
+	final double IVA = 0.21;
+	double pvpFinal;
+	Scanner scan = new Scanner(System.in);
+
+	System.out.print("Precio del producto:");
+	pvp = scan.nextDouble();
+
+	pvpFinal = pvp * IVA + pvp;
+	System.out.println("Precio final con IVA " + pvpFinal);
+
+
+
+
+}
